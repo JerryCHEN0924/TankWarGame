88cc885e08fe57c7c0313962fbea1ca78da00834
JerryCHEN <jk2455892@gmail.com>
2024-08-23 11:56:02 +0800

    加入牆壁並調整背景為黑色 加入工具類,幫助引用資源時免除寫路徑

--- src/main/java/com/jerrychen0924/tankwar/Wall.java
+++ src/main/java/com/jerrychen0924/tankwar/Wall.java
@@ -1,8 +1,31 @@
 package com.jerrychen0924.tankwar;
 
+import java.awt.*;
+
 public class Wall {
 
     private int x;
     private int y;
-    
+    private boolean horizontal;
+    private int bricks;
+
+    public void draw(Graphics graphics){
+        Image brickImage = Tools.getImage("brick.png");
+        if(horizontal){
+            for (int i = 0; i < bricks; i++) {
+                graphics.drawImage(brickImage,x + i * brickImage.getWidth(null),y,null);
+            }
+        } else {
+            for (int i = 0; i < bricks; i++) {
+                graphics.drawImage(brickImage,x ,y + i * brickImage.getHeight(null),null);
+            }
+        }
+    }
+
+    public Wall(int x, int y, boolean horizontal, int bricks) {
+        this.x = x;
+        this.y = y;
+        this.horizontal = horizontal;
+        this.bricks = bricks;
+    }
 }
